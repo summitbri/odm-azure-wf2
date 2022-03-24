e3a97409b9aa748ac11f312d4b460e04be3d781a
summitbri <36425892+summitbri@users.noreply.github.com>
2022-03-24 15:48:11 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "container" {
 }
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
-  default     = "odmv6"
+  default     = "odm-ondemand"
 }
 variable "location" {
   default = "centralus"
