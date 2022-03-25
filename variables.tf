ca92823d3ec79823d62716357d65880b57d92409
summitbri <36425892+summitbri@users.noreply.github.com>
2022-03-25 13:17:49 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,7 +24,7 @@ variable "container" {
 }
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
-  default     = "ODM_TEMP_RSG"
+  default     = "tempRGforODMprocessor"
 }
 variable "location" {
   default = "centralus"
