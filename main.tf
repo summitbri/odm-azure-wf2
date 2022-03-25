b6c01a7f49a09049ceb5dfeeff10b2a483bfaf5b
summitbri <36425892+summitbri@users.noreply.github.com>
2022-03-25 16:26:19 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "azurerm_linux_virtual_machine" "nodeodm" {
 #auto-shutdown vm at night code
 }
 resource "azurerm_dev_test_global_vm_shutdown_schedule" "rg" {
-  virtual_machine_id  = azurerm_linux_virtual_machine.rg.id
+  virtual_machine_id = azurerm_linux_virtual_machine.rg.id
   location           = azurerm_resource_group.rg.location
   enabled            = true
 
